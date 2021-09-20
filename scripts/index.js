9bfe6bb2f8d1def21b058ef9a4e4da8fe378075a
Nikita Vasilinenko <nikitavasilinenko@gmail.com>
2021-09-20 22:04:42 +0300

    first layout

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const priceInput = document.getElementById('price_input');
+const priceInputSlider = document.getElementById('price_input_slider');
+
+priceInput.addEventListener('change', () => {
+  
+});
